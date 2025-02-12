c8b7e34f2217af2142315371b190c93a2f883c37
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-12 11:53:46 -0400

    ADD - Generic App Bar composable

---
+++ app/src/main/java/com/varqulabs/recipexplorer/core/presentation/generics/top_bars/DefaultAppBar.kt
@@ -0,0 +1,56 @@
+package com.varqulabs.recipexplorer.core.presentation.generics.top_bars
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import com.varqulabs.recipexplorer.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DefaultAppBar(
+    title: String,
+    navigationIcon: Int? = R.drawable.baseline_arrow_back_24,
+    modifier: Modifier = Modifier,
+    contentColor: Color = MaterialTheme.colorScheme.primary,
+    backgroundColor: Color = MaterialTheme.colorScheme.surface,
+    onBack: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            if (title.isNotBlank()) {
+                Text(
+                    text = title,
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Medium,
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    color = contentColor,
+                )
+            }
+        },
+        colors = topAppBarColors(containerColor = backgroundColor),
+        navigationIcon = {
+            navigationIcon?.let {
+                IconButton(onClick = onBack) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "Boton atrás",
+                        tint = contentColor,
+                    )
+                }
+            }
+        },
+        modifier = modifier
+    )
+}
