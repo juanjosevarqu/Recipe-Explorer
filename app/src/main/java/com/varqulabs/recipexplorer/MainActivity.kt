17592926a8a9e2dba8e234f32dd5f3284e7216d1
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-11 22:20:40 -0400

    Initial commit app

---
+++ app/src/main/java/com/varqulabs/recipexplorer/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.varqulabs.recipexplorer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.varqulabs.recipexplorer.ui.theme.RecipeExplorerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RecipeExplorerTheme {
+
+            }
+        }
+    }
+}
