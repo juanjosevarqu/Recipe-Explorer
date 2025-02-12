d9cc3b3de0fdc5012609aae8d74cb5001ce24486
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-12 01:27:47 -0400

    EXTRACT - Generic fecth data

--- app/src/main/java/com/varqulabs/recipexplorer/core/domain/DataState.kt
+++ app/src/main/java/com/varqulabs/recipexplorer/core/domain/DataState.kt
@@ -1,5 +1,9 @@
 package com.varqulabs.recipexplorer.core.domain
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.IOException
+
 sealed class DataState<out T> {
 
     data object Loading : DataState<Nothing>()
@@ -8,5 +12,17 @@ sealed class DataState<out T> {
 
     data class Error(val message: String, val code: Int? = null) : DataState<Nothing>()
 
+}
+
+fun <T> fetchData(apiCall: suspend () -> T?): Flow<DataState<T>> = flow {
+    emit(DataState.Loading)
+    try {
+        val result = apiCall() ?: throw Exception("No se encontraron resultados")
+        emit(DataState.Success(result))
+    } catch (e: IOException) {
+        emit(DataState.Error("Error de conexión. Verifica tu internet."))
+    } catch (e: Exception) {
+        emit(DataState.Error("Error: ${e.localizedMessage ?: "Desconocido"}"))
+    }
 }
 
