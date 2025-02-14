9baecbe11187e7f1ba645a229c71e632552dfb98
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-14 09:16:30 -0400

    ADD - limit Font Scale function for accesibility

--- app/src/main/java/com/varqulabs/recipexplorer/MainActivity.kt
+++ app/src/main/java/com/varqulabs/recipexplorer/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.varqulabs.recipexplorer
 
+import android.content.Context
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -28,4 +30,31 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    override fun attachBaseContext(newBase: Context?) {
+            super.attachBaseContext(newBase?.limitFontScale())
+    }
+}
+
+const val MAX_FONT_SCALE = 1.10F
+const val MAX_DPI_SCALE = 1.05F
+
+fun Context.limitFontScale(
+    maxFontScale: Float = MAX_FONT_SCALE,
+    maxDpiScale: Float = MAX_DPI_SCALE,
+): Context {
+    val configuration = resources.configuration
+    val defaultDeviceDensity = DisplayMetrics.DENSITY_DEVICE_STABLE
+    val maximumDpiScale = (defaultDeviceDensity * maxDpiScale).toInt()
+
+    val exceedsFontScale = configuration.fontScale > maxFontScale
+    val exceedsDpiScale = configuration.densityDpi < DisplayMetrics.DENSITY_560 && configuration.densityDpi > maximumDpiScale
+
+    configuration.apply {
+        if (exceedsFontScale) { fontScale = maxFontScale }
+        if (exceedsDpiScale) { densityDpi = maximumDpiScale }
+    }
+
+    return if (exceedsFontScale || exceedsDpiScale) createConfigurationContext(configuration)
+    else this
 }
