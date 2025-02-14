1710a7d834e51b5651904e286978c5c4299e8bd2
juanjosevarqu <135390925+juanjosevarqu@users.noreply.github.com>
2025-02-14 01:29:26 -0400

    REFACTOR - Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,44 +43,65 @@ android {
 
 dependencies {
 
+    implementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+
+    // Core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
 
     // ViewModel
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 
-    // Serialization
-    implementation(libs.kotlinx.serialization.json)
 
-    // hilt
+    // Tests locales (JVM)
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlin.coroutines.test)
+    testImplementation(libs.mockk)
+    testImplementation(libs.turbine)
+
+    // JUnit, Espresso (Instrumented Tests)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.test.runner)
+    androidTestImplementation(libs.test.rules)
+
+    // Compose UI Test
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
-    // retrofit
+    // Hilt testing
+    androidTestImplementation(libs.dagger.hilt.testing)
+    kaptAndroidTest(libs.kapt.test)
+
+    // Hilt testing
+    testImplementation(libs.dagger.hilt.testing)
+    kaptTest(libs.kapt.test)
+
+    // Serialization
+    implementation(libs.kotlinx.serialization.json)
+
+    // Services
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
-
-    // okHTTP
     implementation(libs.okhttp)
     implementation(libs.logging.interceptor)
 
-    // coil
+    // Coil
     implementation(libs.coil)
 
-    // Google Maps
+    // Maps
     implementation(libs.maps.compose)
     implementation(libs.play.services.location)
     implementation(libs.play.services.maps)
 
     // Bundles
     implementation(libs.bundles.compose.libraries)
-
 }
